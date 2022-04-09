1b5f5a260b24e0fbe54c23610a524c6dc226bd17
devsoonote <devsoo0527@gmail.com>
2022-04-09 21:20:03 +0900

    init: create container component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,21 @@ import {NgModule, Optional, SkipSelf} from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContentsComponent } from './components/contents/contents.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   exports: [
     HeaderComponent,
     SidebarComponent,
-    ContentsComponent
+    ContentsComponent,
+    ContainerComponent
   ],
 
   declarations: [
     HeaderComponent,
     SidebarComponent,
-    ContentsComponent
+    ContentsComponent,
+    ContainerComponent
   ]
 })
 export class CoreModule {
