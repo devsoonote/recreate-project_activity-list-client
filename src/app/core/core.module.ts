25b51576bb9a9a6ae2d019659d41018943043176
devsoonote <devsoo0527@gmail.com>
2022-04-16 23:17:46 +0900

    init: set sidebar style

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,8 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ContainerComponent } from './components/container/container.component';
 import { ContentsComponent } from './components/contents/contents.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 import { LoadingComponent } from './components/loading/loading.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
@@ -21,8 +25,15 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     ContentsComponent,
     ContainerComponent,
     LoadingComponent,
+    LogoComponent,
   ],
-  imports: [BrowserModule, MatProgressSpinnerModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    RouterModule,
+  ],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() core: CoreModule) {
