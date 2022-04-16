4495eecc848b330ff353f4e6768f3df62ef7e58b
devsoonote <devsoo0527@gmail.com>
2022-04-16 18:53:29 +0900

    init: component layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from '@core/core.module';
+import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,11 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ContentsComponent } from './components/contents/contents.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { ContainerComponent } from './components/container/container.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   exports: [
@@ -17,7 +20,9 @@ import { ContainerComponent } from './components/container/container.component';
     SidebarComponent,
     ContentsComponent,
     ContainerComponent,
+    LoadingComponent,
   ],
+  imports: [BrowserModule, MatProgressSpinnerModule],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() core: CoreModule) {
